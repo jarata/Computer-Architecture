17f20f129226a0203863aea214d82a228c1b663b
Justin A <jarata@gmail.com>
2019-09-15 20:23:08 -0700

    switching up dictionaries

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,12 +3,19 @@
 import sys
 
 # Global Operation Variables
-HLT = 0b00000001
-LDI = 0b10000010
-PRN = 0b01000111
+operations = {
+    0b00000001: "HLT",
+    0b10000010: "LDI",
+    0b01000111: "PRN",
+    0b10100010: "MUL",
+    0b01000110: "POP",
+    0b01000101: "PUSH",
+    0b01010000: "CALL",
+    0b00010001: "RET"
+}
+
 ADD = 0b10100000
 AND = 0b10101000
-CALL = 0b01010000
 CMP = 0b10100111
 DEC = 0b01100110
 DIV = 0b10100011
@@ -24,14 +31,10 @@ JMP = 0b01010100
 JNE = 0b01010110
 LD = 0b10000011
 MOD = 0b10100100
-MUL = 0b10100010
 NOP = 0b00000000
 NOT = 0b01101001
 OR = 0b10101010
-POP = 0b01000110
 PRA = 0b01001000
-PUSH = 0b01000101
-RET = 0b00010001
 SHL = 0b10101100
 SHR = 0b10101101
 ST = 0b10000100
@@ -51,14 +54,14 @@ class CPU:
         self.ir = 0
         self.running = True
         self.op = {
-            LDI: self.ldi,
-            PRN: self.prn,
-            HLT: self.hlt,
-            POP: self.pop,
-            PUSH: self.push,
-            MUL: self.mul,
-            CALL: self.call,
-            RET: self.ret,
+            operations[0b10000010]: self.ldi,
+            operations[0b01000111]: self.prn,
+            operations[0b00000001]: self.hlt,
+            operations[0b01000110]: self.pop,
+            operations[0b01000101]: self.push,
+            operations[0b10100010]: self.mul,
+            operations[0b01010000]: self.call,
+            operations[0b00010001]: self.ret,
         }
 
     def load(self):
